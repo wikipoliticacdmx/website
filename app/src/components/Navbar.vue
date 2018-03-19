6bb0675b17f203578c8d4456788f72f3ddf3a464
Jorge Andrade <jorandradefig@gmail.com>
2018-03-19 09:53:11 -0600

    Add menu

--- app/src/components/Navbar.vue
+++ app/src/components/Navbar.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="navbar">
     <b-navbar variant="faded" type="light">
-      <b-navbar-brand href="#">Wikipolítica CDMX</b-navbar-brand>
+      <b-container class="bv-example-row">
+        <b-row>
+          <b-col>Inicio</b-col>
+          <b-col>Conócenos</b-col>
+          <b-col>Foro</b-col>
+          <b-col>Únete</b-col>
+          <b-col>Contacto</b-col>
+          <b-button variant="primary">
+            ¡ÚNETE!
+          </b-button>
+          <b-button variant="danger">
+            DONA
+          </b-button>
+        </b-row>
+      </b-container>
     </b-navbar>
   </div>
 </template>
