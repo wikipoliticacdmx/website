80d27eddc7f65c7224b9bee44abdff0e341134fa
Jorge Andrade <jorandradefig@gmail.com>
2018-03-18 22:44:23 -0600

    Add navbar component

---
+++ app/src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="navbar">
+    <b-navbar variant="faded" type="light">
+        <b-navbar-brand href="#">Wikipolítica CDMX</b-navbar-brand>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  props: {
+
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
