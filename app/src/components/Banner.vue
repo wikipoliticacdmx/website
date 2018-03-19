89ebd74ed0e2b89ab54561802639327e5392cfaa
Jorge Andrade <jorandradefig@gmail.com>
2018-03-19 10:34:20 -0600

    Add banner component

---
+++ app/src/components/Banner.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="banner">
+    <b-container>
+      <b-row>
+        <b-col>
+          <p>
+            Hay una nueva política, la posibilidad de un futuro distinto.
+            Una política horizontal, feminista, progresista y sustentable.
+          </p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Banner",
+    props: {
+
+    }
+  };
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
     <Navbar />
+    <Banner />
   </div>
 </template>
 
 <script>
-  import Navbar from "@/components/Navbar.vue";
+  import Navbar from "@/components/Navbar.vue"
+  import Banner from "@/components/Banner.vue"
 
   export default {
     name: "home",
     components: {
-      Navbar
+      Navbar,
+      Banner
     }
   };
 </script>
